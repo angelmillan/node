9aa1572f9435cf04862d1f7be7e5872efc687d4d
Angel Millán <angelmillan@me.com>
2017-10-26 13:05:10 +0200

    node

--- Servicios/node/estadistica.js
+++ Servicios/node/estadistica.js
@@ -1,15 +1,15 @@
 module.exports = function estadistica (coleccion) {
   var _coleccion = coleccion;
   return {
-    numeroElementos : function () {return _coleccion.lenght;},
+    numeroElementos : function () {return _coleccion.length;},
     numeroMasAlto   : function () {
       var numeroAlto = _coleccion[0];
-      if (_coleccion.lenght > 1) {
-        for (var i = 0; i < _coleccion.lenght; i++) {
+      if (_coleccion.length > 1) {
+        for (var i = 0; i < _coleccion.length; i++) {
             if (_coleccion[i] > numeroAlto){
               numeroAlto = _coleccion[i];
             }
-        }
+        } 
       }
       return numeroAlto;
     }
